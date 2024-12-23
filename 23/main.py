ec3726046129df62c2bda3d2cc9370e0a55e81b5
Ryan Vilim <rvilim@character.ai>
2024-12-23 08:16:26 -0500

    setify a bit

--- 23/main.py
+++ 23/main.py
@@ -34,25 +34,31 @@ def find_clique(connections, seed_node):
     seen = set([seed_node])
     # start out with seed node add it to the clique
     # for each of the neighbours of each of the confirmed members, if it is connected to all the confirmed members, add it
-    todo = [c for c in connections[seed_node]]
+    todo = connections[seed_node].copy()
 
     while todo:
         cur = todo.pop()
+
+        # if all the members of the ruunning clique are connected to the current node
         if all(cur in connections[confirmed] for confirmed in clique):
+            # then this node should be a member of the clique too
             clique.add(cur)
-            for cur_neighbour in connections[cur]:
-                if cur_neighbour not in seen:
-                    todo.append(cur_neighbour)
-                    seen.add(cur_neighbour)
+
+            # then we add all the connections of the current node to our todo pile
+            # (we are removing the ones that we've laready looked at, e.g. "- seen")
+            todo |= connections[cur] - seen
+
+            # then add all the connections we just looked at to seen
+            seen |= connections[cur]
 
     return clique
-    # if all(cur in connections[])
 
 
 def two(connections):
     max_clique = set()
     for seed in connections.keys():
         c = find_clique(connections, seed)
+
         if len(c) > len(max_clique):
             max_clique = c
 
