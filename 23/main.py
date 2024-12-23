c2c31012101080c25516fb905d478cfb02c16d86
Ryan Vilim <rvilim@character.ai>
2024-12-23 08:45:26 -0500

    tooo many sets c

--- 23/main.py
+++ 23/main.py
@@ -41,7 +41,7 @@ def find_clique(connections, seed_node):
 
         # if all the members of the running clique are connected to the current node, e.g. if the clique is a subset
         # of the connections of the current node
-        if clique.issubset(connections[cur]):
+        if all(cur in connections[confirmed] for confirmed in clique):
             # then this node should be a member of the clique too
             clique.add(cur)
 
